d9853ea55c4ec4c7c246ded953139be49912e339
Camilo Mora <104514113+KamiMora@users.noreply.github.com>
2022-07-21 03:38:49 -0500

    Initial commit

---
+++ Codigo/Bodega.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class Bodega 
+{
+    private ArrayList<Producto> listaProductos;
+
+    public Bodega() {
+        // TODO implement here
+    }
+
+    public void agregarProducto(Producto p) {
+        
+    }
+
+    public void eliminarProducto(int id) {
+        
+    }
+
+    public void aumentarProducto(int id, int cant) {
+        
+    }
+
+    public void reducirProducto(int id, int cant) {
+        
+    }
+
+    public void modificarPrecio(int id, int precio) {
+        
+    }
+
+    public Producto getProducto(int id) {
+        // TODO implement here
+        return listaProductos.get(0);
+    }
+
+}
---
+++ Codigo/Producto.java
@@ -0,0 +1,58 @@
+public class Producto 
+{
+    public Producto() {
+        
+    }
+    private String tipo;
+    private String nombre;
+    private String marca;
+    private String presentacion;
+    private int cantidad;
+    private int precio;
+    private int id;
+
+    public void Producto() {
+        
+    }
+
+    public void Producto(String tipo, String nombre, String marca, String presentacion, int cant, int precio, int id) {
+        
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public int getCantidad() {
+       return 0;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public int getPrecio() {
+        return 0;
+    }
+
+    public String getTipo() {
+        return "";
+    }
+
+    public String getNombre() {
+        return "";
+    }
+
+    public String getMarca() {
+        return "";
+    }
+
+    public String getPresentacion() {
+        return "";
+    }
+
+    public int getId() {
+        return 0;
+    }
+
+}
