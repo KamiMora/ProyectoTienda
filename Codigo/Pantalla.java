fdcbd5cf3ffb1c394890fe0feb25c30a8e654dad
Camilo Mora <104514113+KamiMora@users.noreply.github.com>
2022-07-26 02:32:56 -0500

    GUI Pantalla implementada
    Error
    java.lang.NullPointerException
    	at Pantalla.cargarTodosProductos(Pantalla.java:258)
    	at Pantalla.<init>(Pantalla.java:41)
    	at Inicio.main(Inicio.java:10)
    
    WTF IS WRONG?

--- Codigo/Pantalla.java
+++ Codigo/Pantalla.java
@@ -1,123 +1,356 @@
+ 
+
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import java.awt.Color;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JComboBox;
 import javax.swing.JButton;
+import javax.swing.BorderFactory;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JScrollPane;
+import javax.swing.Icon;
+import javax.swing.JOptionPane;
+import javax.swing.ImageIcon;
+import javax.swing.ListSelectionModel;
+import java.util.ArrayList;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.UIManager;
 
 public class Pantalla extends JFrame
 {
-    JLabel labelCodigo, labelNombre, labelMarca, labelPresent, labelTipo, labelCant, labelPrecio; //definimos los labels
-    JTextField txtCodigo, txtNombre, txtMarca, txtPresent, txtCant, txtPrecio; //creamos el campo de texto para cada atributo
-    JComboBox comboTipo; //usamos un formato diferente para escoger el tipo
-    JButton btnAgregar, btnActualizar, btnLimpiar, btnEliminar;
-    
+    JTextField txtId, txtCodigo, txtNombre, txtMarca, txtPresent, txtCant, txtPrecio, txtBuscar;
+    JComboBox comboTipo;
+    JButton btnAgregar, btnActualizar, btnLimpiar, btnEliminar, btnFiltrar, btnQuitarFiltro;
+    JTable tablaProductos;
+    DefaultTableModel dtm;
+
     public Pantalla()
     {
-        setBounds(100, 100, 800, 600);
-        setTitle("ProyectoTienda - V2.0");
+        iniciarComponentes();
+        implementarListeners();
+        cargarTodosProductos();
+    }
+
+    public void iniciarComponentes()
+    {
+        setBounds(100, 100, 1200, 550);
+        setTitle("Almacen H6 - V2.0");
         setLayout(null);
-        
-        
+
+        //Creación de páneles
+        JPanel panelBanner = new JPanel();
+        panelBanner.setBounds(0, 0, 1200, 100);
+        panelBanner.setBackground(new Color(64, 0, 196));   
+        panelBanner.setLayout(null);
+        getContentPane().add(panelBanner);
+
         JPanel panelFormulario = new JPanel();
-        panelFormulario.setBounds(0, 0, 400, 600);
-        panelFormulario.setLayout(null); //layout libre
-        getContentPane().add(panelFormulario); //establece un panel general
-        //panelFormulario.setBackground(new Color(100, 100, 100)); // para configurar el fondo
-        
-        
+        panelFormulario.setBounds(5, 105, 455, 402);
+        panelFormulario.setBorder(BorderFactory.createEtchedBorder());
+        panelFormulario.setLayout(null);
+        getContentPane().add(panelFormulario);
+
         JPanel panelTabla = new JPanel();
-        panelTabla.setBounds(400, 0, 400, 600);
+        panelTabla.setBounds(465, 105, 715, 402);
+        panelTabla.setBorder(BorderFactory.createEtchedBorder());
         panelTabla.setLayout(null);
-        getContentPane().add(panelTabla); //panel con una tabla
-        
-        
+        getContentPane().add(panelTabla);
+
+        //Componentes del panel del banner
+        JLabel labelTitulo = new JLabel("Sistema de Inventario y POS");
+        labelTitulo.setBounds(20, 20, 500, 60);
+        labelTitulo.setFont(new Font("Arial", Font.BOLD, 28));
+        labelTitulo.setForeground(Color.WHITE);
+        panelBanner.add(labelTitulo);
+
+        Image imagen = new ImageIcon("imgs\\logo_shop.png").getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT);
+        JLabel labelLogo = new JLabel(new ImageIcon(imagen));
+        labelLogo.setBounds(1050, 10, 84, 80);
+        panelBanner.add(labelLogo);
+
         //Componentes del panel del formulario
-        labelCodigo = new JLabel("Código");
-        labelCodigo.setBounds(10, 20, 100, 30);
-        panelFormulario.add(labelCodigo); //poner labelCodigo en el panel Formulario
+        JLabel labelId = new JLabel("Id");
+        labelId.setBounds(10, 20, 100, 30);
+        panelFormulario.add(labelId);
+
+        txtId = new JTextField();
+        txtId.setBounds(140, 20, 300, 30);
+        txtId.setEditable(false);
+        panelFormulario.add(txtId);
         
+        JLabel labelCodigo = new JLabel("Codigo");
+        labelCodigo.setBounds(10, 60, 100, 30);
+        panelFormulario.add(labelCodigo);
+
         txtCodigo = new JTextField();
-        txtCodigo.setBounds(150, 20, 200, 30);
+        txtCodigo.setBounds(140, 60, 300, 30);
         panelFormulario.add(txtCodigo);
-        
-        labelNombre = new JLabel("Nombre");
-        labelNombre.setBounds(10, 70, 100, 30);
-        panelFormulario.add(labelNombre); 
-        
+
+        JLabel labelNombre = new JLabel("Nombre");
+        labelNombre.setBounds(10, 100, 100, 30);
+        panelFormulario.add(labelNombre);
+
         txtNombre = new JTextField();
-        txtNombre.setBounds(150, 70, 200, 30);
+        txtNombre.setBounds(140, 100, 300, 30);
         panelFormulario.add(txtNombre);
-        
-        labelMarca = new JLabel("Marca");
-        labelMarca.setBounds(10, 120, 100, 30);
-        panelFormulario.add(labelMarca); 
-        
+
+        JLabel labelMarca = new JLabel("Marca");
+        labelMarca.setBounds(10, 140, 100, 30);
+        panelFormulario.add(labelMarca);
+
         txtMarca = new JTextField();
-        txtMarca.setBounds(150, 120, 200, 30);
+        txtMarca.setBounds(140, 140, 300, 30);
         panelFormulario.add(txtMarca);
-        
-        labelPresent = new JLabel("Presentación");
-        labelPresent.setBounds(10, 170, 100, 30);
-        panelFormulario.add(labelPresent); 
-        
+
+        JLabel labelPresent = new JLabel("Presentacion");
+        labelPresent.setBounds(10, 180, 100, 30);
+        panelFormulario.add(labelPresent);
+
         txtPresent = new JTextField();
-        txtPresent.setBounds(150, 170, 200, 30);
+        txtPresent.setBounds(140, 180, 300, 30);
         panelFormulario.add(txtPresent);
-        
+
         JLabel labelTipo = new JLabel("Tipo");
         labelTipo.setBounds(10, 220, 100, 30);
         panelFormulario.add(labelTipo);
-        
-        comboTipo = new JComboBox(); //formato JComboBox para un dropdown
-        comboTipo.addItem("Escoja");
+
+        comboTipo = new JComboBox();
+        comboTipo.addItem("");
         comboTipo.addItem("Aseo");
         comboTipo.addItem("Alimento");
-        comboTipo.setBounds(150, 220, 200, 30);
-        panelFormulario.add(comboTipo); 
-        
-        txtPresent = new JTextField();
-        txtPresent.setBounds(150, 170, 200, 30);
-        panelFormulario.add(txtPresent);
-        
-        labelCant = new JLabel("Cantidad");
-        labelCant.setBounds(10, 270, 100, 30);
-        panelFormulario.add(labelCant); 
-        
+        comboTipo.setBounds(140, 220, 300, 30);
+        UIManager.put("ComboBox.disabledForeground", Color.BLACK);
+        panelFormulario.add(comboTipo);
+
+        JLabel labelCant = new JLabel("Cantidad");
+        labelCant.setBounds(10, 260, 100, 30);
+        panelFormulario.add(labelCant);
+
         txtCant = new JTextField();
-        txtCant.setBounds(150, 270, 200, 30);
+        txtCant.setBounds(140, 260, 300, 30);
         panelFormulario.add(txtCant);
-        
-        labelPrecio = new JLabel("Precio");
-        labelPrecio.setBounds(10, 320, 100, 30);
-        panelFormulario.add(labelPrecio); 
-        
+
+        JLabel labelPrecio = new JLabel("Precio");
+        labelPrecio.setBounds(10, 300, 100, 30);
+        panelFormulario.add(labelPrecio);
+
         txtPrecio = new JTextField();
-        txtPrecio.setBounds(150, 320, 200, 30);
+        txtPrecio.setBounds(140, 300, 300, 30);
         panelFormulario.add(txtPrecio);
-                
-        setVisible(true); // va al final para que todo sea visible
+
+        btnAgregar = new JButton("Agregar");
+        btnAgregar.setBounds(10, 350, 100, 30);
+        panelFormulario.add(btnAgregar);
+
+        btnActualizar = new JButton("Actualizar");
+        btnActualizar.setBounds(120, 350, 100, 30);
+        panelFormulario.add(btnActualizar);
+
+        btnLimpiar = new JButton("Limpiar");
+        btnLimpiar.setBounds(230, 350, 100, 30);
+        panelFormulario.add(btnLimpiar);
+
+        btnEliminar = new JButton("Eliminar");
+        btnEliminar.setBounds(340, 350, 100, 30);
+        panelFormulario.add(btnEliminar);
+
+        //Componentes del panel de la tabla
+        Object[][] datos = null;
+        String[] columnas = {"Id", "Codigo", "Nombre", "Marca", "Presentación", "Tipo", "Cant", "Precio"};     
+        dtm= new DefaultTableModel(datos, columnas);
+
+        tablaProductos = new JTable(dtm);
+        tablaProductos.setPreferredScrollableViewportSize(new Dimension(715, 290));
+        tablaProductos.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        tablaProductos.setFillsViewportHeight(true);
+        JScrollPane scroll = new JScrollPane(tablaProductos);
+
+        JPanel contenidoTabla = new JPanel();
+        contenidoTabla.setBounds(0, 0, 715, 290);
+        contenidoTabla.setLayout(new GridLayout(1,0));
+        contenidoTabla.add(scroll);
+        panelTabla.add(contenidoTabla);
+
+        JLabel labelBuscar = new JLabel("Buscar por: ");
+        labelBuscar.setBounds(10, 350, 100, 30);
+        panelTabla.add(labelBuscar);
+
+        txtBuscar = new JTextField();
+        txtBuscar.setBounds(140, 350, 300, 30);
+        panelTabla.add(txtBuscar);
+
+        btnFiltrar = new JButton("Filtrar");
+        btnFiltrar.setBounds(460, 350, 100, 30);
+        panelTabla.add(btnFiltrar);
+
+        btnQuitarFiltro = new JButton("Quitar filtro");
+        btnQuitarFiltro.setBounds(570, 350, 120, 30);
+        panelTabla.add(btnQuitarFiltro);
+
+        setVisible(true);
+    }
+
+    public void implementarListeners()
+    {
+        btnAgregar.addActionListener(new ActionListener() 
+        {
+            public void actionPerformed(ActionEvent e) 
+            {
+                agregarProducto();
+            }
+        });
+
+        btnLimpiar.addActionListener(new ActionListener() 
+        {
+            public void actionPerformed(ActionEvent e) 
+            {
+                limpiarFormulario();
+            }
+        });
+            
+        btnQuitarFiltro.addActionListener(new ActionListener() 
+        {
+            public void actionPerformed(ActionEvent e) 
+            {
+                cargarTodosProductos();   
+            }
+        });
+
+        btnFiltrar.addActionListener(new ActionListener() 
+        {
+            public void actionPerformed(ActionEvent e) 
+            {
+                cargarProductosFiltrados();
+            }
+        });
+
+        tablaProductos.getSelectionModel().addListSelectionListener(new ListSelectionListener() 
+        {
+            public void valueChanged(ListSelectionEvent e) 
+            {
+                if (tablaProductos.getSelectedRowCount() > 0) 
+                {
+                    int row = tablaProductos.getSelectedRow();
+                    int codigobarras = Integer.parseInt(dtm.getValueAt(row, 1).toString());
+                    cargarProductoEnFormulario(codigobarras);   
+                }
+            }
+        });
+    }
+
+    public void cargarTodosProductos()
+    {
+        tablaProductos.clearSelection();
+        txtBuscar.setText("");
+        Bodega bodega = new Bodega();
+        ArrayList<Producto> productos = bodega.getListaProductos();
+        dtm.setRowCount(0);
+        for(Producto p : productos)
+        {
+            Object[] row = { p.getId(), p.getCodigoBarras(), p.getNombre(), p.getMarca(), p.getPresentacion(), p.getTipo(), p.getCantidad(), p.getPrecio()};
+            dtm.addRow(row);
+        }
+    }
+
+    public void cargarProductosFiltrados()
+    {
+        tablaProductos.clearSelection();
+        if (txtBuscar.getText().equals(""))
+        {
+            JOptionPane.showMessageDialog(this, "Debe colocar un filtro para la búsqueda");
+            return;
+        }
+        Bodega bodega = new Bodega();
+        ArrayList<Producto> productos = bodega.buscarProductos(txtBuscar.getText());
+        dtm.setRowCount(0);
+        for(Producto p : productos)
+        {
+            Object[] row = {p.getId(), p.getCodigoBarras(), p.getNombre(), p.getMarca(), p.getPresentacion(), p.getTipo(), p.getCantidad(), p.getPrecio()};
+            dtm.addRow(row);
+        }
+    }
+
+    public void agregarProducto()
+    {
+        if (comboTipo.getSelectedItem().equals("") || txtCodigo.getText().equals("")  || txtNombre.getText().equals("") || txtMarca.getText().equals("") || txtPresent.getText().equals("") || txtCant.getText().equals("") || txtPrecio.getText().equals("")) 
+        {
+            JOptionPane.showMessageDialog(this, "Debe completar la información del producto");
+            return;
+        }
+        if (!txtId.getText().equals(""))
+        {
+            JOptionPane.showMessageDialog(this, "Ingrese información de un nuevo producto");
+            return;
+        }
+        
+        int id = 0;
+        int codigobarras = Integer.parseInt(txtCodigo.getText());
+        String nombre = txtNombre.getText();
+        String marca = txtMarca.getText();
+        String presentacion = txtPresent.getText();
+        String tipo = comboTipo.getSelectedItem().toString();
+        int cant = Integer.parseInt(txtCant.getText());
+        int precio = Integer.parseInt(txtPrecio.getText());
+        Producto p = new Producto(id, codigobarras, tipo, nombre, marca, presentacion, cant, precio );
+        Bodega bodega = new Bodega();
+        bodega.anadirProducto(p);
+        limpiarFormulario();
+        cargarTodosProductos();
+    }
+    
+    public void limpiarFormulario()
+    {
+        txtId.setText("");
+        txtCodigo.setText("");
+        txtNombre.setText("");
+        txtMarca.setText("");
+        txtPresent.setText("");
+        comboTipo.setSelectedItem("");
+        txtCant.setText("");
+        txtPrecio.setText("");
+        activarFormulario(true);
+    }
+    
+    
+    public void cargarProductoEnFormulario(int codigobarras) 
+    {
+        Bodega b = new Bodega();
+        Producto p = b.getProducto(codigobarras);
+        txtId.setText(p.getId() + "");
+        txtCodigo.setText(p.getCodigoBarras() + "");
+        txtNombre.setText(p.getNombre());
+        txtMarca.setText(p.getMarca());
+        txtPresent.setText(p.getPresentacion());
+        comboTipo.setSelectedItem(p.getTipo());
+        txtCant.setText(p.getCantidad() + "");
+        txtPrecio.setText(p.getPrecio() + ""); 
+        activarFormulario(false);
+    }
+    
+    public void activarFormulario(boolean activar)
+    {
+        txtCodigo.setEditable(activar);
+        txtNombre.setEditable(activar);
+        txtMarca.setEditable(activar);
+        txtPresent.setEditable(activar);
+        comboTipo.setEnabled(activar);
     }
 }
 
 
 
 
-/** GUI
-     * Frame/Ventana(nombre, botones, ancho, alto, ubicación)
-         
-         * Labels
-         * Text field/text box
-         * DropDown
-         * Buttons
-         * Layout: con coordenadas x, y, ancho, alto (n, n, n, n) || libre (null)
-             * JFrame: panel general
-             * Paneles: Divisiones dentro de un frame
-                    * Panel bounds: con respecto al frame
-             * Frame Bounds: alineación con respecto a la pantalla
-         * Color: RGB (n, n, n) en donde n = 0-255
 
- * POE: Programación Orientada a Eventos
- * 
- */
+
+
+
